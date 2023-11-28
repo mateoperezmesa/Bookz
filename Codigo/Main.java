f20ff44f15f60cdfffa62dd40f5e6f6ae5cbb281
mateoperezmesa <132848028+mateoperezmesa@users.noreply.github.com>
2023-11-28 09:37:43 -0500

    Update Main.java Version 9:40 am 28/11

--- Codigo/Main.java
+++ Codigo/Main.java
@@ -1,18 +1,46 @@
-import java.util.*;
+import java.util.Scanner;
+
+import javax.swing.SwingUtilities;
 
 public class Main {
-    public static void main(String[] args) {
-        // Ejemplo de uso
-        Scanner scan = new Scanner(System.in);
-        String nuevoUsuario = scan.nextLine();
-        String contrasena = scan.nextLine();
 
-        if (GestorBaseDatos.usuarioDisponible(nuevoUsuario)) {
-            GestorBaseDatos.registrarUsuario(nuevoUsuario, contrasena);
-            System.out.println("Registro exitoso para " + nuevoUsuario);
-        } else {
-            System.out.println("El username no está disponible.");
-        }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Esto es para establecer la coneccion
+        String dbURL = "jdbc:mysql://localhost:3306/bookz";
+        String username = "root";
+        String password = "1234";
+
+        GestorBaseDatos gestorBD = new GestorBaseDatos(dbURL, username, password);
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Ventana();
+            }
+        });
+
+        // Aquí puedes cambiar el username y la contraseña según tus necesidades
+        System.out.println("Usuario con @unal.edu.co");
+        // String correo = scan.nextLine();
+        // String nuevoUsuario = Usuario.extraerUsername(correo);
+
+        /*
+         * while (nuevoUsuario == null) {
+         * correo = scan.nextLine();
+         * nuevoUsuario = Usuario.extraerUsername(correo);
+         * }
+         */
+
+        System.out.println("Contraseña");
+        // String contrasena = scan.nextLine();
+
+        // Registrar un nuevo usuario
+        // gestorBD.registrarUsuario(nuevoUsuario, contrasena);
+        // gestorBD.obtenerInformacionUsuario(nuevoUsuario);
+        // gestorBD.mostrarlibros();
+        gestorBD.obtenerInformacionLibro("n");
 
         scan.close();
     }
